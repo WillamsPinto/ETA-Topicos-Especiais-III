e11fac8a42c8592ca8c29948b8c3b32d1a5be619
WillamsPinto <willamspinto18@gmail.com>
2023-11-25 18:49:07 -0300

    Adição de alguns testes

--- src/main/java/queries/Countries.java
+++ src/main/java/queries/Countries.java
@@ -7,8 +7,6 @@ import static io.restassured.RestAssured.given;
 
 public class Countries {
 
-    public final String query1 = "{\"query\":\"query consultarPais {\\n  country(code: \\\"AR\\\"){\\n    name\\n    capital\\n    continent{\\n      code\\n    }\\n    languages{\\n      code\\n      name\\n      native\\n    }\\n    subdivisions{\\n      code\\n      emoji\\n      name\\n    }\\n  }\\n}\",\"variables\":{}}";
-
     public static Response getCountryQuery(String countryCode, RequestSpecification requestSpec){
         return given().spec(requestSpec)
                         .body("{\"query\":\"query consultarPais {\\n  country(code: \\\""+countryCode+"\\\"){\\n    name\\n    capital\\n    continent{\\n      code\\n    }\\n    languages{\\n      code\\n      name\\n      native\\n    }\\n    subdivisions{\\n      code\\n      emoji\\n      name\\n    }\\n  }\\n}\",\"variables\":{}}")
--- src/test/java/CountriesQueryTest.java
+++ src/test/java/CountriesQueryTest.java
@@ -1,15 +1,56 @@
 import io.restassured.response.Response;
+import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 import queries.Countries;
 
-import static io.restassured.RestAssured.given;
-
 public class CountriesQueryTest extends BaseTest{
+
+    private Response response = null;
 
     @Test
     public void shouldReturnStatus200() {
 
-        Response response = Countries.getCountryQuery("AR", requestSpec);
-        response.then().assertThat().statusCode(200).log().body();
+        response = Countries.getCountryQuery("AR", requestSpec);
+        response.then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void CheckReturnName(){
+        response = Countries.getCountryQuery("AR", requestSpec);
+        response.then().
+                assertThat().
+                body("data.country.name", is ("Argentina"));
+    }
+
+    @Test
+    public void CheckReturnCapital(){
+        response = Countries.getCountryQuery("AR", requestSpec);
+        response.then().
+                assertThat().
+                body("data.country.capital", is ("Buenos Aires"));
+    }
+
+    @Test
+    public void CheckReturnContinentCode(){
+        response = Countries.getCountryQuery("AR", requestSpec);
+        response.then().
+                assertThat().
+                body("data.country.continent.code", is ("SA"));
+    }
+
+    @Test
+    public void CheckReturnFirstLanguage(){
+        response = Countries.getCountryQuery("AR", requestSpec);
+        response.then().
+                assertThat().
+                body("data.country.languages.code[0]", is ("es"));
+    }
+
+    @Test
+    public void CheckReturnCountLanguage(){
+        response = Countries.getCountryQuery("AR", requestSpec);
+        response.then().
+                assertThat().
+                body("data.country.languages.size()", is (2));
     }
 }
