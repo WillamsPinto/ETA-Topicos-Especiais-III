51aec7931f85fc8cab559a3846009c627f5a751c
WillamsPinto <willamspinto18@gmail.com>
2023-11-25 16:30:57 -0300

    Adicação da classe BaseTest

--- src/test/java/CountriesQueryTest.java
+++ src/test/java/CountriesQueryTest.java
@@ -2,14 +2,14 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class CountriesQueryTest {
+public class CountriesQueryTest extends BaseTest{
 
     @Test
     public void shouldReturnStatus200() {
-        given().header("Content-Type", "application/json")
+        given().spec(requestSpec)
                 .body("{\"query\":\"query consultarPais {\\n  country(code: \\\"AR\\\"){\\n    name\\n    capital\\n    continent{\\n      code\\n    }\\n    languages{\\n      code\\n      name\\n      native\\n    }\\n    subdivisions{\\n      code\\n      emoji\\n      name\\n    }\\n  }\\n}\",\"variables\":{}}")
 
-        .when().post("https://countries.trevorblades.com/")
+        .when().post()
         .then().assertThat().statusCode(200).log().body();
     }
 }
